bc4a77d008995c972be846da1e728bff9208c5f1
Ellie <dpfla9144@naver.com>
2024-03-14 12:41:26 +0900

    ADD : tmxor

---
+++ study/자료구조/스택.js
@@ -0,0 +1,12 @@
+function stack() {
+  const stack = [];
+  stack.push("1");
+  stack.push("2");
+  stack.push("3");
+  stack.push("4");
+  stack.push("5");
+  stack.pop();
+  console.log(stack);
+}
+
+stack();
